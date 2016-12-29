5a055bb1724034dbc5285e93e097d4bba7b723ea
thanksdanny <hj-07@163.com>
2016-12-29 23:53:53 +0800

    20161229

---
+++ chap11持有对象/AsListInterference.java
@@ -0,0 +1,20 @@
+import java.tuil.*;
+
+class Snow{}
+class Powder extends Snow{}
+class Light extends Powder{}
+class Heavy extends Powder{}
+class Crusty extends Snow{}
+class Slush extends Snow{}
+
+public class AsListInterference {
+	public static void main(String[] args) {
+		List<Snow> snow1 = Arrays.asList(new Crusty(), new Slush(), new Powder());
+
+		List<Snow> snow3 = new ArrayList<Snow>();
+		Collections.addAll(snow3, new Light(), new Heavy());
+
+		List<Snow> snow4 = Arrays.<Snow>asList(new Light(), new Heavy());
+	}
+	
+}
